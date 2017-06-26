7146ceefc7f4df699646e5050e125aedc3493dc1
dbachko <dbachko@gmail.com>
2017-06-26 13:19:42 -0400

    Do not bundle external modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+var nodeExternals = require('webpack-node-externals');
+
+
 module.exports = {
   entry: './handler.js',
   target: 'node',
+  externals: [nodeExternals()], // ignore all modules in node_modules folder
   module: {
     loaders: [{
       test: /\.js$/,
