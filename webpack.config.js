470e91e4b270957f22f9a132a45fc30a26c588bb
dbachko <dbachko@gmail.com>
2017-12-19 22:33:48 -0500

    Upgrades npm modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,33 @@
-var nodeExternals = require('webpack-node-externals');
+const dotenv = require('dotenv');
+// const nodeExternals = require('webpack-node-externals');
+// const webpack = require('webpack');
 
+dotenv.config({ path: `${__dirname}/.env` });
 
 module.exports = {
-  entry: './handler.js',
+  entry: ['./handler.js'],
   target: 'node',
-  externals: [nodeExternals()], // ignore all modules in node_modules folder
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }]
+  // externals: [nodeExternals()], // ignore all modules in node_modules folder
+  externals: ['.env'],
+  resolve: {
+    extensions: ['.js']
   },
-  plugins: []
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     TEST: JSON.stringify(process.env.TEST),
+    //   },
+    // }),
+  ]
 };
