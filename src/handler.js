7baecf005f028a6d489e8303193a58d0e4afd438
dbachko <dbachko@gmail.com>
2020-02-05 21:15:30 -0500

    Updates code repo

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,16 @@
-export const hello = (event, context, callback) => {
-  const message = 'Go Serverless! Your function executed successfully!';
-  const response = {
+export const hello = async event => {
+  return {
     statusCode: 200,
-    body: JSON.stringify({
-      input: event,
-      message,
-    }),
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event
+      },
+      null,
+      2
+    )
   };
 
-  callback(null, response);
-};
-
-export default {
-  hello,
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
